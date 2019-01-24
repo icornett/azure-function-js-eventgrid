0607efdde42396118d2a6ce94623d45b28039755
Ian Cornett <icornett@redapt.com>
2019-01-23 19:09:11 -0800

    Update debugging for dns task

--- addDnsRecord/index.js
+++ addDnsRecord/index.js
@@ -1,20 +1,42 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-console */
 /* eslint-disable func-names */
-// const msRestAzure = require('ms-rest-azure');
-// const DNSManagment = require('azure-arm-dns');
-// const NetworkMgmtClient = require('azure-arm-network');
-// const ResourceMgmtClient = require('azure-arm-resource').ResourceManagementClient;
+const msRestAzure = require('ms-rest-azure');
+const DNSManagment = require('azure-arm-dns');
+const NetworkMgmtClient = require('azure-arm-network');
+const ResourceMgmtClient = require('azure-arm-resource').ResourceManagementClient;
+const ComputeMgmtClient = require('azure-arm-compute');
 
 function addDnsRecord(resourceUri, subscriptionId) {
   console.log(`Resource ID: ${resourceUri}, Subscription ID: ${subscriptionId}`);
-/*  msRestAzure
-    .loginWithAppServiceMSI()
+  // let virtualMachine;
+  const resourceData = resourceUri.split('/');
+  console.log(`Resource Group:\t${resourceData[4]}`);
+  console.log(`VM Name:\t${resourceData[resourceData.length - 1]}`);
+  msRestAzure
+    .loginWithUsernamePassword('icornett@redapt.com', '#######')
     .then((credentials) => {
-
-    }); */
+      const computeClient = new ComputeMgmtClient(credentials, subscriptionId);
+      computeClient.virtualMachineId.get(
+        resourceData[4], // Resource Group Name
+        resourceData[resourceData.length - 1], // Virtual Machine Name
+        (err, result) => {
+          if (err) {
+            console.log('An error occurred while getting VM info');
+            console.log(err.toString());
+          } else {
+            // virtualMachine = result;
+            console.log(JSON.stringify(result));
+          }
+        },
+      );
+    })
+    .catch(
+      (err) => { console.log(`${'an error occurred!\t'}${err.message}`); },
+    );
 }
 
-module.exports = async function (context, eventGridEvent) {
+module.exports = async function run(context, eventGridEvent) {
   const SubscriptionValidationEvent = 'Microsoft.EventGrid.SubscriptionValidationEvent';
   const WriteSuccess = 'Microsoft.Resources.ResourceWriteSuccess';
   const VmWrite = 'Microsoft.Compute/virtualMachines/write';
@@ -28,7 +50,8 @@ module.exports = async function (context, eventGridEvent) {
     };
   } else if (eventGridEvent.eventType === WriteSuccess) {
     if (eventData.operationName === VmWrite) {
-      addDnsRecord(eventGridEvent.resourceUri);
+      console.log('Attempting to create DNS Record');
+      addDnsRecord(eventData.resourceUri, eventData.subscriptionId);
     }
   }
 };
--- rmDnsRecord/index.js
+++ rmDnsRecord/index.js
@@ -1,29 +1,32 @@
-const msRestAzure = require('ms-rest-azure');
-const DNSManagment = require('azure-arm-dns');
+// const msRestAzure = require('ms-rest-azure');
+// const DNSManagment = require('azure-arm-dns');
 
-module.exports = async function (context, eventGridEvent) {
-    const SubscriptionValidationEvent = "Microsoft.EventGrid.SubscriptionValidationEvent";
-    const DeleteSuccess = "Microsoft.Resources.ResourceDeleteSuccess";
-    const VmWrite = "Microsoft.Compute/virtualMachines/write";
+// module.exports = async function (context, eventGridEvent) {
+//     const SubscriptionValidationEvent = "Microsoft.EventGrid.SubscriptionValidationEvent";
+//     const DeleteSuccess = "Microsoft.Resources.ResourceDeleteSuccess";
+//     const VmWrite = "Microsoft.Compute/virtualMachines/write";
 
-    let eventData = eventGridEvent.data;
+//     let eventData = eventGridEvent.data;
 
-    if (eventGridEvent.eventType === SubscriptionValidationEvent) {
-        context.log('Got SubscriptionValidation event data, validationCode: ' + eventData.validationCode + ', topic: ' + eventGridEvent.topic);
-        context.res = {
-            validationResponse: eventData.validationCode
-        };
-    } else if (eventGridEvent.eventType == WriteSuccess) {
-        if (eventData.operationName == VmWrite){
-            rmDnsRecord(eventGridEvent.resourceUri)
-        }
-    }
-};
+//     if (eventGridEvent.eventType === SubscriptionValidationEvent) {
+//         context.log('Got SubscriptionValidation event data, validationCode: '
+//                      + eventData.validationCode
+//                      + ', topic: '
+//                      + eventGridEvent.topic);
+//         context.res = {
+//             validationResponse: eventData.validationCode
+//         };
+//     } else if (eventGridEvent.eventType == WriteSuccess) {
+//         if (eventData.operationName == VmWrite){
+//             rmDnsRecord(eventGridEvent.resourceUri)
+//         }
+//     }
+// };
 
-function rmDnsRecord(resourceUri, subscriptionId) {
-    msRestAzure
-        .loginWithAppServiceMSI()
-        .then(credentials => {
+// function rmDnsRecord(resourceUri, subscriptionId) {
+//     msRestAzure
+//         .loginWithAppServiceMSI()
+//         .then(credentials => {
 
-        })
-}
+//         })
+// }
