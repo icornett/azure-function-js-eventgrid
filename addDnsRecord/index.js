5d19d3ac13efdd4a926732898d4e563c4d2ec770
Ian Cornett <icornett@redapt.com>
2019-01-25 17:18:25 -0800

    build out basic logic, fails w/ TypeError on IsRecordExisting

--- addDnsRecord/index.js
+++ addDnsRecord/index.js
@@ -1,50 +1,103 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-console */
-/* eslint-disable func-names */
-const msRestAzure = require('ms-rest-azure');
-// const DNSManagment = require('azure-arm-dns');
-const NetworkMgmtClient = require('azure-arm-network');
-// const ResourceMgmtClient = require('azure-arm-resource').ResourceManagementClient;
-const ComputeMgmtClient = require('azure-arm-compute');
 
-async function addDnsRecord(resourceUri, subscriptionId, context) {
-  const vmPath = resourceUri.split('/');
-  // const dnsClient = new DNSManagment(credentials, subscriptionId);
-  msRestAzure.interactiveLogin()
-    .then((credentials) => {
-      const computeClient = new ComputeMgmtClient(credentials, subscriptionId);
-      computeClient.virtualMachines.get(
-        vmPath[4], // Resource Group Name
-        vmPath[vmPath.length - 1], // Virtual Machine Name
-        { expand: 'instanceView' },
-        (err, info) => {
-          if (err) {
-            context.log('An error occurred while getting VM info!\n');
-            context.log(err.message);
-          }
-          context.log(`Got VM Information for VM \n${JSON.stringify(info)}`);
-          return { vmInfo: info, credential: credentials };
-        },
-        (err) => {
-          context.log(`An error occurred while trying to get VM information! The error was:\t${err.message}`);
-        },
-      );
+const async = require('async');
+const ComputeManagementClient = require('azure-arm-compute');
+const DNSManagement = require('azure-arm-dns');
+const msRestAzure = require('ms-rest-azure');
+const NetworkManagementClient = require('azure-arm-network');
+// eslint-disable-next-line prefer-destructuring
+const ResourceManagementClient = require('azure-arm-resource').ResourceManagementClient;
+
+function checkPrimary(ipConfiguration) {
+  return ipConfiguration.primary === true;
+}
+
+async function getNicInfo(vmInfo, credentials, subscriptionId) {
+  const networkClient = new NetworkManagementClient(credentials, subscriptionId);
+  const nic0Path = vmInfo.networkProfile.networkInterfaces[0].id.split('/');
+  return networkClient.networkInterfaces.get(
+    nic0Path[4], // Resource ID
+    nic0Path[nic0Path.length - 1], // NIC ID
+  );
+}
+
+async function getZonesFromRg(dnsClient, rgInfo) {
+  return dnsClient.zones.listByResourceGroup(rgInfo.name);
+}
+
+async function getVmInfo(vmPath, credentials, subscriptionId) {
+  const computeClient = new ComputeManagementClient(credentials, subscriptionId);
+  return computeClient.virtualMachines.get(
+    vmPath[4], // Resource Group Name
+    vmPath[vmPath.length - 1], // Virtual Machine Name
+  );
+}
+
+// Get List of A records in the Zone
+async function getZoneInfo(nicInfo, dnsClient, zoneList, rgName) {
+  const zoneName = zoneList[0].name;
+  return dnsClient.recordSets.listByType(rgName, zoneName, 'A');
+}
+
+async function isRecordExisting(rgName, vmName, zoneName, dnsClient) {
+  return dnsClient.recordSets.get(rgName, zoneName, vmName, 'A');
+}
+
+async function setDnsARecord(nicInfo, dnsClient, zoneList, zoneRecords, rgName, vmName, context) {
+  const zoneName = zoneList[0].name;
+  const ipAddress = nicInfo.ipConfigurations.filter(checkPrimary);
+  isRecordExisting(rgName, vmName, zoneName, dnsClient)
+    .then((exists) => {
+      if (exists && zoneList.length > 0) {
+        throw new Error(`Record exists:\n${JSON.stringify(exists)}`);
+      }
+
+      dnsClient.recordSets.createOrUpdate(rgName, zoneName, vmName, 'A', {
+        aRecords: [
+          ipAddress[0].privateIPAddress,
+        ],
+      });
     })
-    .then((vmInfo, credential) => {
-      const networkClient = new NetworkMgmtClient(credential, subscriptionId);
-      const nic0Path = vmInfo.networkProfiles.networkInterfaces[0].split('/');
-      networkClient.networkInterfaces.get(
-        nic0Path[4], // Resource ID
-        nic0Path[nic0Path.length - 1], // NIC ID
-        (err, info) => {
-          if (err) {
-            console.log(err);
-          }
-          context.log(`Got NIC info for NIC\n${JSON.stringify(info)}`);
-          return info;
-        },
-      );
-    });
+    .catch(err => context.log(`Error occurred while setting the DNS A Record!\nThe error was:\t${err.message}\n${JSON.stringify(err)}`));
+}
+
+async function addDnsRecord(resourceUri, subscriptionId, zoneName, context) {
+  let nicInfo;
+  let rgInfo;
+  let vmInfo;
+  let zoneRecords;
+  let zoneList;
+
+  const vmPath = resourceUri.split('/');
+  const rgName = vmPath[4];
+  const vmName = vmPath[vmPath.length - 1];
+  const credentials = await msRestAzure.interactiveLogin();
+  const dnsClient = new DNSManagement(credentials, subscriptionId);
+  const resourceClient = new ResourceManagementClient(credentials, subscriptionId);
+
+  async.series([
+    rgInfo = await resourceClient.resourceGroups.get(rgName)
+      .catch(e => context.log(e.message)),
+    zoneList = await getZonesFromRg(dnsClient, rgInfo)
+      .catch(e => context.log(e.message)),
+    vmInfo = await getVmInfo(vmPath, credentials, subscriptionId, context)
+      .catch(e => context.log(e.message)),
+    nicInfo = await getNicInfo(vmInfo, credentials, subscriptionId)
+      .catch(e => context.log(e.message)),
+    zoneRecords = await getZoneInfo(nicInfo, dnsClient, zoneList, rgInfo.name, context)
+      .catch(e => context.log(e.message)),
+    setDnsARecord(
+      nicInfo,
+      dnsClient,
+      zoneList,
+      zoneRecords,
+      rgInfo.name,
+      vmName,
+      context,
+    )
+      .catch(e => context.log(e.message)),
+  ]);
 }
 
 module.exports = async function run(context, eventGridEvent) {
